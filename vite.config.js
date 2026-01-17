26cf4e4e0455f6c728a81972a2c83bb756e5bc57
poirazis <poirazis@gmail.com>
2026-01-17 15:06:34 +0200

    Enable css code splitting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   css: {
@@ -14,29 +15,45 @@ export default defineConfig({
     svelte({
       preprocess: vitePreprocess(),
       compilerOptions: { dev: false },
-      emitCss: false,
+      emitCss: true,
       onwarn: (warning, handler) => {
         if (warning.code !== "unused-export-let") {
           handler(warning);
         }
       },
     }),
+    cssInjectedByJsPlugin(),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
       name: "SupercomponentsShared",
-      fileName: "index",
+      fileName: (format) => `index.${format === "umd" ? "umd.cjs" : "js"}`,
+      formats: ["es", "umd"],
     },
-    cssCodeSplit: false,
+    cssCodeSplit: true,
     rollupOptions: {
       external: ["svelte", "@budibase/bbui"],
-      output: {
-        globals: {
-          svelte: "Svelte",
-          "@budibase/bbui": "bbui",
+      output: [
+        {
+          format: "es",
+          entryFileNames: "index.js",
+          dir: "dist",
+          globals: {
+            svelte: "Svelte",
+            "@budibase/bbui": "bbui",
+          },
         },
-      },
+        {
+          format: "umd",
+          name: "SupercomponentsShared",
+          entryFileNames: "index.umd.cjs",
+          globals: {
+            svelte: "Svelte",
+            "@budibase/bbui": "bbui",
+          },
+        },
+      ],
     },
   },
   resolve: {
